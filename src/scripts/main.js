8e0b35941506b0ea5800adee25b32207efe2eec8
Dmytro Kasatkin <amid19091995@gmail.com>
2026-02-20 12:09:00 +0200

    fix: address review comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const form = document.querySelector('.contacts__form');
+
+if (form) {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    form.reset();
+  });
+}
